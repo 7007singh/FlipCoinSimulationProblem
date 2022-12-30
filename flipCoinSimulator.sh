717d4fcf68b5c6d54f376e5fce258fe0821857bd
7007singh <shw12021982@gmail.com>
2022-12-31 03:04:35 +0530

    Added: Winner and WinPoints

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,6 +1,6 @@
 echo "Welcome to Flip Coin Simulation Program..!"
 
-read -p "Enter the number for flipping a coin " n
+read -p "Enter the number <=50 for flipping a coin " n
 
 for ((i=1; i<=$n; i++))
 do
@@ -8,9 +8,28 @@ flip=$((RANDOM%2))
 if [ $flip -eq 1 ]
 then
 echo "heads"
-echo $((1+head++))
+h=$((1+head++))
+echo $h
 else
 echo "tails"
-echo $((1+tail++))
+t=$((1+tail++))
+echo $t
 fi
+
+if [ $h -eq 21 ]
+then
+echo "head won"
+echo "winning points "$((21-$t))
+break
+elif [ $t -eq 21 ]
+then
+break
+echo "tail won"
+echo "Winning points "$((21-$h))
+break
+else [ $h -eq $t ]
+echo "tie"
+fi
+
 done
+
