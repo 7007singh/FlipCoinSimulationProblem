0100a3939687f1f707198c55ab3e45dbfea37408
7007singh <shw12021982@gmail.com>
2022-12-31 00:38:45 +0530

    Added: Multiple Times Filliping a Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,16 @@
 echo "Welcome to Flip Coin Simulation Program..!"
-flip=$(($((RANDOM%10))%2))
+
+read -p "Enter the number for flipping a coin " n
+
+for ((i=1; i<=$n; i++))
+do
+flip=$((RANDOM%2))
 if [ $flip -eq 1 ]
 then
 echo "heads"
+echo $((1+head++))
 else
 echo "tails"
+echo $((1+tail++))
 fi
+done
