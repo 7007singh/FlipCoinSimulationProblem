c628c09926a17b4c1063740d1cfcd6436c458769
7007singh <shw12021982@gmail.com>
2022-12-31 14:28:08 +0530

    Added: Tie game continues till the difference of minimum 2 points is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,35 +1,32 @@
 echo "Welcome to Flip Coin Simulation Program..!"
 
-read -p "Enter the number <=50 for flipping a coin " n
-
-for ((i=1; i<=$n; i++))
+target_count=21
+minimum_difference=2
+head=0
+tail=0
+flips_count=0
+while [ 1 ]
 do
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-echo "heads"
-h=$((1+head++))
-echo $h
-else
-echo "tails"
-t=$((1+tail++))
-echo $t
-fi
+    ((flips_count++))
+    coin=$((RANDOM%2))
+    if [ $coin -eq 0 ]
+    then
+       # echo "Heads"
+        ((head++))
+    else
+       # echo "Tails"
+        ((tail++))
+    fi
+    diff_bt_hc_tc=$(($head-$tail))
 
-if [ $h -eq 21 ]
-then
-echo "head won"
-echo "winning points "$((21-$t))
-break
-elif [ $t -eq 21 ]
-then
-break
-echo "tail won"
-echo "Winning points "$((21-$h))
-break
-else [ $h -eq $t ]
-echo "tie"
-fi
-
+if [ $head -eq $target_count ] && [ $diff_bt_hc_tc -ge $minimum_difference ]
+    then
+         echo "Heads won by $diff_bt_hc_tc points"
+    break
+    elif [ $tail -eq $target_count ] && [ $diff_bt_hc_tc -ge $minimum_difference ]
+    then
+         echo "Tails won by $diff_bt_hc_tc points"
+    break
+    fi
 done
-
+echo "The Heads count is $head and Tails Count is $tail"
